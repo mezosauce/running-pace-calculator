07beb87cbf3d7ee3fb2870641effcbad82018c0c
ChrisTatlonghari <Chris.Tatlonghari@fox.com>
2023-09-10 14:32:02 -0400

    Add project files.

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,76 @@
+﻿namespace running_pace_calculator.Pages;
+
+public partial class Index
+{
+    private double _distanceSliderValue = 12.5;
+    private string[] _runningDistanceLabels = new string[] { "2mi", "5k", "4mi", "5mi", "10k", "15k", "10mi", "Half", "Full"};
+    private TimeOnly? _raceTime = null;
+    private int _minutes = 9;
+    private int _seconds = 30;
+
+    protected override Task OnInitializedAsync()
+    {
+        UpdateRaceTime();
+
+        return base.OnInitializedAsync();
+    }
+
+    private void OnRaceDistanceChanged(double value)
+    {
+        _distanceSliderValue = value;
+
+        UpdateRaceTime();
+    }
+
+    private void OnPaceMinutesChanged(int value)
+    {
+        _minutes = value;
+
+        UpdateRaceTime();
+    }
+
+    private void OnPaceSecondsChanged(int value)
+    {
+        _seconds = value;
+
+        UpdateRaceTime();
+    }
+
+    private void UpdateRaceTime()
+    {
+        double miles = MapValueToMiles(_distanceSliderValue);
+
+        TimeSpan timeSpan = TimeSpan.FromMinutes(_minutes) + TimeSpan.FromSeconds(_seconds);
+
+        double ticks = timeSpan.Ticks * miles;
+
+        _raceTime = new TimeOnly((long)ticks);
+    }
+
+    private double MapValueToMiles(double sliderValue)
+    {
+        switch(sliderValue)
+        {
+            case 0:
+                return 2;
+            case 12.5:
+                return 3.1;
+            case 25:
+                return 4;
+            case 37.5:
+                return 5;
+            case 50:
+                return 6.2;
+            case 62.5:
+                return 9.3;
+            case 75:
+                return 10;
+            case 87.5:
+                return 13.1;
+            case 100:
+                return 26.1;
+            default:
+                return 0;
+        }
+    }
+}
---
+++ Theme/WebsiteTheme.cs
@@ -0,0 +1,74 @@
+﻿using MudBlazor;
+
+namespace running_pace_calculator.Theme;
+public class WebsiteTheme : MudTheme
+{
+    public WebsiteTheme()
+    {
+        Palette = new PaletteLight()
+        {
+            Primary = "#f90c0c",
+            Secondary = "#9E9E9E",
+            Tertiary = "#e6e6e6",
+            TertiaryContrastText = "#000",
+            Background = "#eee",
+            Surface = "#f5f3f4",
+            AppbarBackground = "#353535",
+            AppbarText = "rgba(255,255, 255, 0.7)",
+            DrawerBackground = "#f5f3f4",
+            DrawerText = "rgba(0, 0, 0, 0.9)",
+            Success = "#06d79c",
+            Divider = "#000000cc",
+            DividerLight = "#e0e0e0ff",
+            White = "#ffffff",
+            TextDisabled = "#777777",
+            ErrorLighten = "ffe6e6",
+            SuccessLighten = "eaffe6",
+        };
+
+        PaletteDark = new PaletteDark()
+        {
+            Primary = "#2e9884",
+            PrimaryContrastText = "f5f5dc",
+            Secondary = "#408ec6",
+            Tertiary = "#424242",
+            TertiaryContrastText = "#fff",
+            Black = "#171717",
+            Background = "#1e1e2d",
+            BackgroundGrey = "#0e0e0e",
+            Surface = "rgba(37,37,37, 1.0)",
+            DrawerBackground = "rgba(14,14,14, 1.0)",
+            DrawerText = "rgba(255,255,255, 0.50)",
+            DrawerIcon = "rgba(255,255,255, 0.50)",
+            AppbarBackground = "rgba(28,28,28, 1.0)",
+            AppbarText = "rgba(255,255,255, 0.70)",
+            TextPrimary = "f5f5dc",
+            TextSecondary = "rgba(255,255,255, 0.50)",
+            ActionDefault = "#adadb1",
+            ActionDisabled = "rgba(255,255,255, 0.26)",
+            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+            Divider = "rgba(255,255,255, 0.12)",
+            DividerLight = "rgba(255,255,255, 0.06)",
+            TableLines = "rgba(255,255,255, 0.12)",
+            LinesDefault = "rgba(255,255,255, 0.12)",
+            LinesInputs = "rgba(255,255,255, 0.3)",
+            TextDisabled = "rgba(255,255,255, 0.2)",
+            White = "ffffff",
+            ErrorLighten = "660000",
+            SuccessLighten = "004D00"
+        };
+
+        Typography = new Typography()
+        {
+            Default = new Default()
+            {
+                FontFamily = new[] { "", "Ubuntu", "Helvetica", "Arial", "sans-serif" },
+                FontSize = ".875rem",
+                FontWeight = 400,
+                LineHeight = 1.43,
+                LetterSpacing = ".01071em"
+            },
+        };
+    }
+}
+
